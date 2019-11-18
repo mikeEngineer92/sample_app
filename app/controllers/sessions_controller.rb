71d54cf47fd71d407dadf11a91f793155b53bcf0
EC2 Default User <ec2-user@ip-172-31-45-222.ap-northeast-1.compute.internal>
2019-11-18 13:17:03 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       # DBにトークン保存
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       # Failure
       flash.now[:danger] = 'Invalid email/password combination'
