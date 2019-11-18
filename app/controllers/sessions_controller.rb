21d5a1b7966f40963ca908ebd1d47dd30bf4c6da
EC2 Default User <ec2-user@ip-172-31-45-222.ap-northeast-1.compute.internal>
2019-11-18 06:25:05 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Success
       log_in user
+      # DBにトークン保存
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       # Failure
@@ -16,7 +18,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,11 +4,25 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  # ユーザーを永続的に復元できるようにする
+  def remember(user)
+    user.remember # DBにremember_digestを入れる
+    # permanentを使うと永続的にクッキーが生き残る
+    cookies.permanent.signed[:user_id] = user.id # 暗号化
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
   
@@ -17,8 +31,16 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
