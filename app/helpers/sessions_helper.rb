35284fb31de8e38424b20d12cfda7ba719187dc2
EC2 Default User <ec2-user@ip-172-31-45-222.ap-northeast-1.compute.internal>
2019-11-19 07:55:06 +0000

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,55 +1,51 @@
 module SessionsHelper
-
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  # ユーザーを永続的に復元できるようにする
+
+  # ユーザーを永続的に復元できるようになった
   def remember(user)
-    user.remember # DBにremember_digestを入れる
-    # permanentを使うと永続的にクッキーが生き残る
-    cookies.permanent.signed[:user_id] = user.id # 暗号化
+    user.remember # => DB: remember_digest
+    cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
-  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
-  def current_user?(user)
-    user == current_user
-  end
 
-  # 現在ログイン中のユーザーを返す (いる場合)
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-  
-  # ユーザーがログインしていればtrue、その他ならfalseを返す
-  def logged_in?
-    !current_user.nil?
-  end
-  
-  # 永続的セッションを破棄する
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-  
-  # 現在のユーザーをログアウトする
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+
+    elsif (user_id = cookies.signed[:user_id])
+      # raise
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+   # 現在のユーザーをログアウトする
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
   # 記憶したURL (もしくはデフォルト値) にリダイレクト
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '02592a9a8a6b40d793bceced6d385c89.vfs.cloud9.ap-northeast-1.amazonaws.com' # ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
